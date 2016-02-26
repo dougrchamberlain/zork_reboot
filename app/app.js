6c9e0ade8c5d5071ca052b8faa9b5618ea3f68e2
doug chamberlain <dchamberlain@identifi.net>
2016-02-26 16:41:23 -0500

    this patch adds hitting yourself

--- app/app.js
+++ app/app.js
@@ -21,258 +21,36 @@ angular.module("myApp", [
         controller: "appController",
         controllerAs: "vm"
     });
-}]).controller("appController", ["$scope", "$resource", "_", "mapService", "itemService", "playerService", function ($scope, $resource, _, mapService, itemService, playerService) {
+}]).controller("appController", ["$scope", "$resource", "_","gameService", function ($scope, $resource, _, gameService) {
     var vm = this;
-    vm.commands = [];
-    vm.status = []
-    vm.player = {};
-    vm.player.inventory = itemService.getInventory().inventory;
 
-    var startingItems = [{
-        name: "lamp",
-        life: 20,
-        canActivate: true,
-        canCarry: true,
-        details: {description: "Wanko brand darkness removal tool."}
-    },
-        {
-            name: "jar",
-            canCarry: true,
-            container: {isOpen: false, contents: {}},
-            details: {description: "long description"},
-            text: "A Mason jar is a molded glass jar used in home canning to preserve food. The mouth of the jar has screw threads on its outer perimeter to accept a metal ring (or \"band\"). The band, when screwed down, presses a separate stamped steel disc-shaped lid against the rim of the jar. An integral rubber ring on the underside of the lid creates a hermetic seal to the jar. The bands and lids usually come with new jars, and bands and lids are also sold separately; while the bands are reusable, the lids are intended for single use when canning.                While largely supplanted by other methods, such as the tin can and plastic containers, for commercial mass production, they are still commonly used in home canning.                The Mason jar was invented and patented in 1858 by Philadelphia tinsmith John Landis Mason[1][2] (1832–1902). Among other common names for them are Ball jars, after Ball Corporation, an early and prolific manufacturer of the jars; fruit jars for a common content; and simply glass canning jars reflecting their material."
-        },
-        {name: "butterfly", canCarry: true, player: playerService}
-    ]
+    vm.player = gameService.createPlayer("Doug");
 
-
-    vm.map = mapService.load({rooms: {}});
-    mapService.createRooms([1, 2, 3, 4, 5, 6, 7, 8, 9].map(function (item) {
-        itemService.loadRoom([], "room " + item);
-        return "room " + item;
-    }));
-
-    itemService.loadRoom(startingItems, "room 1");
-
-    mapService.getMap().rooms["room 1"].exits.north = "room 2";
-    mapService.getMap().rooms["room 2"].exits.east = "room 1";
-    mapService.setLocation("room 1");
-
-
-    vm.location = mapService.currentLocation();
-
-
-    vm.isWinner = function () {
-        return vm.location.name == "room 9" && vm.hasLitLamp();
-    }
-
-    vm.hasLitLamp = function () {
-        var result = itemService.getInventory().inventory["lamp"] || itemService.getAvailable().items[vm.location.name].contents["lamp"];
-        if (result) {
-            result.state = result.state || "off";
-        }
-        return result.state == "on";
-    }
-
-    var clearService = function (item) {
-        vm.status = [];
-        // return;
-    };
-
-    vm.possibleExits = function (items) {
-        var result = {};
-        angular.forEach(items, function (value, key) {
-            if (value) {
-                result[key] = value;
-            }
-        });
-        return result;
-    }
-
-    var possibleActions = [
-        {
-            name: ["look", "l"], needsObject: false, callback: function (item) {
-            if (item.length == 0) {
-                itemService.look();
-            }
-            angular.forEach(item, function (i) {
-                itemService.look(i);
-            });
-        }
-        },
-        {
-            name: ["put"], callback: function (items) {
-            itemService.put(items[0]).into(items[2]);
-        }
-        },
-        {
-
-            name: ["north", "south", "west", "east", "northeast", "southeast", "southwest", "northwest"],
-            needsObject: false,
-            callback: function (item, words) {
-                item = item == "go" ? words[0] : item;
-                vm.status = [];
-                if (vm.location.exits[item]) {
-                    mapService.move(item);
-                    vm.location = mapService.currentLocation();
-                    itemService.loadRoom([], vm.location.name);
-                    itemService.look();
-                }
-                else {
-                    vm.status.push("You can't go that way");
-                }
-
-                // return;
-            }
-        },
-        {
-            name: ["read"], callback: function (item) {
-            itemService.read(item[0]);
-        }
-        },
-        {
-            name: ["clear"], needsObject: false, callback: clearService
-        },
-        {
-            name: ["open"], needsObject: false, callback: function (item) {
-            itemService.open(item[0]);
-
-        }
-        },
-
-        {
-            name: ["take", "get"], needsObject: false, callback: function (item) {
-            angular.forEach(item, function (i) {
-                itemService.take(i);
-            });
-        }
-        },
-        {
-            name: ["drop", "d"], needsObject: false, callback: function (item) {
-            angular.forEach(item, function (i) {
-                itemService.drop(i);
-            });
-        }
-        },
-
-
-        {
-            name: ["activate", "turn", "use"], callback: function (item, words) {
-            var lamp = itemService.getInventory().inventory["lamp"] || itemService.getAvailable().items["lamp"];
-            if (lamp) {
-                lamp.state = "on";
-            }
-        }
-        },
-        {
-            name: ["fight", "attack", "kill"], callback: function (item, words) {
-
-            playerService.health.takeHit(100);
-
-            if (playerService.health.isDead()) {
-                console.log("you are dead");
-                console.log(itemService.getAvailable().items["room 1"].contents["butterfly"].player.health.amount)
-            }
-            // return;s
-        }
-        },
-
-        {
-            name: ["desc", "describe", "examine"], callback: function (item, words) {
-            var details = itemService.look(words[0]);
-
-            vm.status.push(details.description);
-
-        }
-        }];
-
-    vm.parseCommand = function (command) {
-
-        //is it an item command?
-        var itemCommand = command.match(/^(put|tie|attack|throw|turn|break|attack|kill|put|look)\s(.+\s?)\s(?:with|to|at|in)\s(\w+)$/i);
-        var moveCommand = command.match(/^(?:go\s)?([nsew]$|(ne|nw|se|sw|north|south|east|west|northwest|southwest|northeast|southeast|down|up|d|u))$/i);
-
-
-        var words = command.split(/(?:\s+|the|this|that|with)/g);
-        var actions = [];
-
-        possibleActions.forEach(function (action) {
-            action.name.forEach(function (name) {
-                name = name.toLowerCase();
-                if (_.contains(words, name)) {
-                    actions.push(action);
-                    words.splice(_.findWhere(words, name), 1);
-                    action.callback(words);
-                }
-            })
-        });
-
-        _.forEach(words, function (w) {
-            w = w.toLowerCase()
-        });
-
-        return {actions: actions, objects: words, isValid: actions.length > 0}
-
-    };
-
-    vm.parseMessage = function (action, obj) {
-        vm.status.push("You " + action.name + " at " + obj)
-    };
 
 
     vm.processCommand = function (command) {
-        var returnedValues = vm.parseCommand(command);
 
-        if (returnedValues.isValid) {
-            vm.commands.push(command.toString());
-            vm.command = "";
-        }
-        else {
-            vm.status.push("I beg your pardon?");
-        }
+      vm.player[command.split(' ')[0]];
     }
 
     vm.queueCommand = function (command, event) {
         if (event.keyCode == 13) {
             vm.status = [];
-            vm.processCommand(command);
+            vm.processCommand(command.toLowerCase());
         }
 
 
     }
 
 
-}]).service("playerService", ["$controller", function ($controller) {
-    function Player() {
-        this.greet = function () {
-            return "hello";
-        }
+}]).factory("gameService", ["_", "$controller", function (_, $controller) {
+    return {
+        createPlayer: function (name) {
+            var player = $controller("playerController");
+            player.name = name;
+            return player;
 
-
-        this.health = $controller("healthController");
-    }
-
-    return new Player();
-}]).controller("lightController", [function () {
-    var vm = this;
-
-    vm.amount = 100;
-
-    vm.turnOn = function () {
-        if (vm.amount > 0) {
-            vm.state = "on";
         }
     }
-
-    vm.turnOff = function () {
-        if (vm.amount > 0) {
-            vm.state = "off";
-        }
-    }
-
-    vm.state = function () {
-        return vm.state;
-    }
-
-
 }])
+;
--- app/app_test.js
+++ app/app_test.js
@@ -1,5 +1,4 @@
 describe('appController', function () {
-    var playerConfigObj;
     beforeEach(function () {
 
 
@@ -10,34 +9,54 @@ describe('appController', function () {
 
     })
 
-    var $rootScope, $scope, $controller, createController, _, mapService, player;
+    var $rootScope, $scope, $controller, createController, _, mapService, gameService;
 
-    beforeEach(inject(function (_$rootScope_, _$controller_, ___, _mapService_, _playerService_) {
+    beforeEach(inject(function (_$rootScope_, _$controller_, ___, _mapService_, _gameService_) {
         // The injector unwraps the underscores (_) from around the parameter names when matching
         $rootScope = _$rootScope_;
         $scope = $rootScope.$new();
         $controller = _$controller_;
         _ = ___;
         mapService = _mapService_;
-        player = _playerService_;
+        gameService = _gameService_;
 
+        createController = function(){
+            return $controller("appController",{$scope: $scope, gameService: gameService})
+        }
 
     }));
 
+    fdescribe("app tests", function () {
 
-    describe("Move Commands", function () {
-        it("Should create an instance of a player", function () {
-            //var p = player;
+        it("should create a new player instance", function () {
+            var vm = createController();
+            var player = vm.player;
 
-            expect(player.greet()).toBe("hello");
+
+            expect(player.name).toBe("Doug");
+            expect(player.health.current).toBe(100);
         });
 
+        it("should check players starting health and score", function () {
+            var vm = createController();
+            var player = vm.player;
+
+
+            expect(player.health.current).toBe(100);
+            expect(player.score.current).toBe(0);
+        });
+
+        it("should attack an enemy and get a score!", function () {
+            var vm = createController();
+            var enemy = $controller("enemyController");
+            var player = vm.player;
+
+            player.attack(enemy,10);
+
+            expect(enemy.health.current.toBe)
+            expect(player.score.current).toBe(10);
+        });
     });
 
-    describe("Win Conditions", function () {
-
-
-    });
 
 })
-;
