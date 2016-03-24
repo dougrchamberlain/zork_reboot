8fade12d35ebff2e65151b48283c153543a89303
doug chamberlain <dchamberlain@identifi.net>
2016-03-24 16:21:51 -0400

    this patch ads some more services

--- app/app.js
+++ app/app.js
@@ -15,28 +15,66 @@ angular.module("myApp", [
 
 }]).config(["$stateProvider", "$urlRouterProvider", function ($stateProvider, $urlRouterProvider) {
     $urlRouterProvider.otherwise('/');
-}]).controller("appController", ["$resource", "_", function ( $resource, _) {
+}]).controller("appController", ["$resource", "_","lookService", function ( $resource, _,lookService) {
     var vm = this;
     vm.inputText = "";
-    var commandList = ["look"];
+    var commandList = [{keyword: "look", delegate: lookService.lookAt }];
 
     var processCommand = function (command) {
         var text = command.split(' ');
         var keyword = text[0].toLowerCase();
         console.log(keyword);
-        var foundCommand = _.filter(commandList,function(value){
-            return value == keyword;
-        });
+        var foundCommand = _.filter(commandList,function(commandObject){
+            return keyword === commandObject.keyword;
+        })[0];
 
-        return {isValid: angular.isDefined(foundCommand) }
+        console.log(foundCommand);
+        var isValid =  angular.isDefined(foundCommand);
+
+        angular.extend(foundCommand, {isValid: isValid});
+
+        return foundCommand;
     };
 
     vm.readCommand = function (event) {
         if (event.keyCode == 13) {
             vm.command = processCommand(vm.inputText);
+            if(vm.command.isValid){
+                console.log(vm.command.delegate);
+                vm.command.delegate.apply(null, []);
+                //TODO: try and execute the command against the service, passing in the needed objects
+            }
         }
     };
 
-}]);
+}]).factory("lookService", ["_","gameService", function(_, gameService){
+
+    var lookAt = function(item){
+        if(item && item.describe) {
+            item.describe();
+        }
+        else{
+            gameService.currentRoom().describe();
+        }
+    }
+
+    return {
+        lookAt: lookAt
+    }
+}]).factory("gameService", function(){
+    var _currentRoom;
+
+    var setRoom = function(room){
+        _currentRoom = room;
+    }
+    var currentRoom = function(){
+        return _currentRoom;
+    };
+
+    return {
+        currentRoom: currentRoom,
+        setRoom: setRoom
+    }
+});
 
 //TODO: make an input processing service, that when it finds the right method, it tries to execute it, and passes back if it can't
--- app/app_test.js
+++ app/app_test.js
@@ -1,33 +1,77 @@
 /**
  * Created by doug on 3/23/2016.
  */
-describe("Command Interpreter",function(){
+describe("Command Interpreter", function () {
 
-    var $controller;
+    var $controller, lookService, gameService;
 
-    beforeEach(function(){
+    beforeEach(function () {
         module("myApp");
     });
 
-    beforeEach(inject(function(_$controller_){
+    beforeEach(inject(function (_$controller_, _lookService_, _gameService_) {
         $controller = _$controller_;
+        lookService = _lookService_;
+        gameService = _gameService_;
+
+
     }));
 
-    it("should find valid commands",function(){
-        var vm = $controller("appController");
-        vm.inputText = "Look";
+    describe("look at input commands", function () {
+        beforeEach(function () {
+            spyOn(lookService, "lookAt").and.callFake(function (item) {
+                console.log("item is described")
+            });
+        });
 
-        vm.readCommand({keyCode: 13});
 
-        expect(vm.command.isValid).toBe(true);
-    });
+        it("should find valid commands", function () {
+            var vm = $controller("appController");
+            var enterKeyEvent = {keyCode: 13};
+            vm.inputText = "Look";
 
-    it("should find valid commands",function(){
-        var vm = $controller("appController");
-        vm.inputText = "Look";
+            vm.readCommand(enterKeyEvent);
 
-        vm.readCommand({keyCode: 13});
+            expect(vm.command.isValid).toBe(true);
+        });
 
-        expect(vm.command.isValid).toBe(true);
+        it("should try and execute command if command is valid", function () {
+            var vm = $controller("appController");
+
+            vm.inputText = "Look";
+
+            //Act
+            vm.readCommand({keyCode: 13});
+
+            //Assert
+            expect(lookService.lookAt).toHaveBeenCalledWith();
+        });
+    })
+
+    describe("lookService", function () {
+        it("should look at a thing and describe if it can be described.", function () {
+            var item = {describe: function(){}};
+
+
+            spyOn(item, "describe").and.returnValue("the description");
+            lookService.lookAt(item);
+            expect(item.describe).toHaveBeenCalled();
+        });
+
+        fit("should look at a thing and describe if it can be described.", function () {
+            var room = {describe: function(){ return "this room is neat";}};
+            spyOn(room, "describe").and.callThrough();
+
+           gameService.setRoom(room);
+
+
+            lookService.lookAt();
+            expect(room.describe).toHaveBeenCalled();
+        });
+
+
+
     });
 });
+
+
