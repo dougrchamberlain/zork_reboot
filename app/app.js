5df255a80ee637acc50c1fcb0c74cd49071f24b7
doug chamberlain <dchamberlain@identifi.net>
2016-03-25 15:02:41 -0400

    legit looking around the starting room.

--- app/app.js
+++ app/app.js
@@ -15,32 +15,46 @@ angular.module("myApp", [
 
 }]).config(["$stateProvider", "$urlRouterProvider", function ($stateProvider, $urlRouterProvider) {
     $urlRouterProvider.otherwise('/');
-}]).controller("appController", ["$resource", "_","lookService","gameService","outputService", function ( $resource, _,lookService,gameService,outputService) {
+}]).controller("appController", ["$resource", "_", "lookService", "gameService", "outputService", function ($resource, _, lookService, gameService, outputService) {
     var vm = this;
     vm.inputText = "";
     vm.outputText = outputService.out;
 
     var startingRoom = $resource("starting-room.json");
 
-    startingRoom.get( function(payload){
+    startingRoom.get(function (payload) {
         console.log(payload);
         gameService.setRoom(payload);
     });
 
-    var commandList = [{keyword: "look", delegate: lookService.lookAt }];
+    var commandList = [{
+        keyword: "look", delegate: lookService.lookAt,
+        params: {
 
+            0: {args: []},
+
+            1: {args: []}
+        }
+    }];
 
 
     var processCommand = function (command) {
+        command = command.toLowerCase();
+
         var text = command.split(' ');
-        var keyword = text[0].toLowerCase();
-        console.log(keyword);
-        var foundCommand = _.filter(commandList,function(commandObject){
+        var keyword = text.splice(0, 1)[0]; //get keyword
+        var args = text;
+
+        var foundCommand = _.filter(commandList, function (commandObject) {
             return keyword === commandObject.keyword;
         })[0];
 
-        console.log(foundCommand);
-        var isValid =  angular.isDefined(foundCommand);
+        var isValid = angular.isDefined(foundCommand) && angular.isDefined(foundCommand.params[args.length]);
+
+        _.forEach(args, function (arg) {
+            foundCommand.args.push(arg);
+        });
+
 
         angular.extend(foundCommand, {isValid: isValid});
 
@@ -50,41 +64,43 @@ angular.module("myApp", [
     vm.readCommand = function (event) {
         if (event.keyCode == 13) {
             vm.command = processCommand(vm.inputText);
-            if(vm.command.isValid){
+            if (vm.command.isValid) {
                 outputService.writeOut("\n\r>> " + vm.command.keyword + "\n\r");
-                vm.command.delegate.apply(null, []);
+                vm.command.delegate.apply(null, vm.command.args);
+                vm.scroll = 1;
                 //TODO: try and execute the command against the service, passing in the needed objects
             }
         }
     };
 
-}]).factory("lookService", ["_","gameService", function(_, gameService){
+}]).factory("lookService", ["_", "gameService", function (_, gameService) {
 
-    var lookAt = function(item){
-        if(item && item.describe) {
+    var lookAt = function (item) {
+        if (item && item.describe) {
+            console.log("try to describe" + item);
             item.describe();
         }
-        else{
-           gameService.describe(gameService.currentRoom());
+        else {
+            gameService.describe(gameService.currentRoom());
         }
     }
 
     return {
         lookAt: lookAt
     }
-}]).factory("gameService", ["outputService", function(outputService){
+}]).factory("gameService", ["outputService", function (outputService) {
     var _currentRoom;
 
-    var setRoom = function(room){
+    var setRoom = function (room) {
         _currentRoom = room;
     }
-    var currentRoom = function(){
+    var currentRoom = function () {
         return _currentRoom;
     };
 
-    var describe = function(item){
+    var describe = function (item) {
         //TODO: a whole bunch of logic to write out descriptions
-        console.log(item.description);
+
         outputService.writeOut(item.description);
     }
 
@@ -93,19 +109,35 @@ angular.module("myApp", [
         setRoom: setRoom,
         describe: describe
     }
-}]).factory("outputService", function(){
-        var output = "";
+}]).factory("outputService", function () {
+    var output = "";
 
 
-        return {
-            writeOut: function(text){
-                output = output + text + "\n\r";
-            },
-            out: function(){
-                console.log(output);
-                return output;
-            }
+    return {
+        writeOut: function (text) {
+            output = output + text + "\n\r";
+        },
+        out: function () {
+
+            return output;
         }
+    }
+
+}).directive('showTail', function () {
+    return {
+        scope: {
+            scroll: "="
+        },
+        link: function (scope, elem, attr) {
+            scope.$watch(function () {
+                    return scope.scroll;
+                },
+                function (e) {
+                    scope.scroll = 0;
+                    elem[0].scrollTop = elem[0].scrollHeight;
+                });
+        }
+    }
 
 });
 
